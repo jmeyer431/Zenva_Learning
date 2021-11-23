12b2ae7d880e7fd76a77b257fdfbd3fbe6223fa1
Justin Meyer <justinm@a-t-g.com>
2021-11-23 15:31:05 -0600

    JS Course - Lesson 12 'Comparison Operators'

--- JS Course/script.js
+++ JS Course/script.js
@@ -1,14 +1,37 @@
-let direction = 'diagonal';
+let item = 'window';
 
-if(direction == 'left'){
-    console.log('red planet');
-} 
-else if(direction == 'right'){
-    console.log('purple planet');
+if(item != 'engine'){
+    console.log('not engine!');
 }
-else if(direction == 'backwards'){
-    console.log('back home');
+
+let score = 5;
+
+if(score >= 60){
+    console.log('pass');
+}
+else if(score < 10){
+    console.log('meet the teacher');
 }
 else{
-    console.log('ship crashes');
+    console.log('not pass');
+}
+
+let isEngine = item == 'engine';
+console.log(isEngine);
+
+if(isEngine){
+
+}
+
+let balance = 100;
+let itemPrice = 101;
+
+// 1. check balance
+if (balance >= itemPrice){
+    balance -= itemPrice;
+    console.log('item purchased');
+    console.log(balance);
+}
+else{
+    console.log('not enough balance');
 }
