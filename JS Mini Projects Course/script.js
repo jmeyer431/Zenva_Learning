e806456ac8c149c0b8ac5620c0d55851b4012bfb
Justin Meyer <justinm@a-t-g.com>
2021-12-06 15:50:01 -0600

    JS MiniProject-Lesson 10 App Structure Part 2

--- JS Mini Projects Course/script.js
+++ JS Mini Projects Course/script.js
@@ -2,30 +2,31 @@ let question = {
     title: 'gato',
     alternatives: ['dog', 'cat', 'bird', 'fish'],
     correctAnswer: 1
-}
+};
 
-function start(){
-    let alts = document.querySelectorAll('.alternative');
+let app = {
+    start: function(){
+        let alts = document.querySelectorAll('.alternative');
     
-    alts.forEach(function(element, index){
-        element.addEventListener('click', function(){
-            console.log('check correct answer');
+        alts.forEach(function(element, index){
+            element.addEventListener('click', function(){
+                console.log('check correct answer');
+            });
         });
-    });
-
-    showQuestion(question);
-}
-
-function showQuestion(q){
-    let titleDiv = document.getElementById('title');
     
-    titleDiv.textContent = q.title;
-
-    let alts = document.querySelectorAll('.alternative');
+        this.showQuestion(question);
+    },
+    showQuestion: function(q){
+        let titleDiv = document.getElementById('title');
     
-    alts.forEach(function(element, index){
-        element.textContent = q.alternatives[index];
-    });
-}
+        titleDiv.textContent = q.title;
+    
+        let alts = document.querySelectorAll('.alternative');
+        
+        alts.forEach(function(element, index){
+            element.textContent = q.alternatives[index];
+        });
+    }
+};
 
-start();
+app.start();
