fc3e491e41b202b1a4c6437db575d39c494268c4
Justin Meyer <justinm@a-t-g.com>
2021-12-06 15:33:09 -0600

    JS Mini-Project Course-Lesson 3 Showing a Question

---
+++ JS Mini Projects Course/script.js
@@ -0,0 +1,19 @@
+let question = {
+    title: 'gato',
+    alternatives: ['dog', 'cat', 'bird', 'fish'],
+    correctAnswer: 1
+}
+
+function showQuestion(q){
+    let titleDiv = document.getElementById('title');
+    
+    titleDiv.textContent = q.title;
+
+    let alts = document.querySelectorAll('.alternative');
+    
+    alts.forEach(function(element, index){
+        element.textContent = q.alternatives[index];
+    });
+}
+
+showQuestion(question);
