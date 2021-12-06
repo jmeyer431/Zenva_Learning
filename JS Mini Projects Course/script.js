29fd9e71ccc95cc9e88095179d9386c039b1e351
Justin Meyer <justinm@a-t-g.com>
2021-12-06 16:16:34 -0600

    JS MiniProject-Lesson 15 Multiple Questions

--- JS Mini Projects Course/script.js
+++ JS Mini Projects Course/script.js
@@ -1,18 +1,31 @@
-let question = {
-    title: 'gato',
-    alternatives: ['dog', 'cat', 'bird', 'fish'],
-    correctAnswer: 1
-};
+let questions = [
+    {
+        title: 'gato',
+        alternatives: ['dog', 'cat', 'bird', 'fish'],
+        correctAnswer: 1
+    },
+    {
+        title: 'ave',
+        alternatives: ['mouse', 'hamster', 'lizard', 'bird'],
+        correctAnswer: 3
+    },
+    {
+        title: 'rata',
+        alternatives: ['cat', 'fish', 'rat', 'shark'],
+        correctAnswer: 2
+    },
+    {
+        title: 'mosca',
+        alternatives: ['fly', 'puma', 'fish', 'dog'],
+        correctAnswer: 0
+    }
+];
 
 let app = {
     start: function(){
-        let alts = document.querySelectorAll('.alternative');
+        this.currentPosition = 0;
 
-        // alts.forEach(function(element, index){
-        //     element.addEventListener('click', function(){
-        //         this.checkAnswer(index);
-        //     }.bind(this));
-        // }.bind(this));
+        let alts = document.querySelectorAll('.alternative');
 
         alts.forEach((element, index) =>{
             element.addEventListener('click', () =>{
@@ -20,12 +33,10 @@ let app = {
             });
         });
     
-        this.showQuestion(question);
+        this.showQuestion(questions[this.currentPosition]);
     },
 
     showQuestion: function(q){
-        this.currentQuestion = q
-
         let titleDiv = document.getElementById('title');
     
         titleDiv.textContent = q.title;
@@ -38,11 +49,23 @@ let app = {
     },
 
     checkAnswer: function(userSelected){
-        if(this.currentQuestion.correctAnswer == userSelected){
+        let currentQuestion = questions[this.currentPosition];
+        if(currentQuestion.correctAnswer == userSelected){
             console.log('Correct!');
         }
         else{
             console.log('Incorrect!');
+        }
+
+        this.increasePosition();
+        this.showQuestion(questions[this.currentPosition]);
+    },
+
+    increasePosition: function(){
+        this.currentPosition++;
+
+        if(this.currentPosition == questions.length){
+            this.currentPosition = 0;
         }
     }
 };
