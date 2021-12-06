7f3a02432cfa9762267e0edde2aef48cb1022334
Justin Meyer <justinm@a-t-g.com>
2021-12-06 16:00:11 -0600

    JS MiniProject-Lesson 12 Context/Checking Answer

--- JS Mini Projects Course/script.js
+++ JS Mini Projects Course/script.js
@@ -7,16 +7,25 @@ let question = {
 let app = {
     start: function(){
         let alts = document.querySelectorAll('.alternative');
-    
-        alts.forEach(function(element, index){
-            element.addEventListener('click', function(){
-                console.log('check correct answer');
+
+        // alts.forEach(function(element, index){
+        //     element.addEventListener('click', function(){
+        //         this.checkAnswer(index);
+        //     }.bind(this));
+        // }.bind(this));
+
+        alts.forEach((element, index) =>{
+            element.addEventListener('click', () =>{
+                this.checkAnswer(index);
             });
         });
     
         this.showQuestion(question);
     },
+
     showQuestion: function(q){
+        this.currentQuestion = q
+
         let titleDiv = document.getElementById('title');
     
         titleDiv.textContent = q.title;
@@ -26,6 +35,15 @@ let app = {
         alts.forEach(function(element, index){
             element.textContent = q.alternatives[index];
         });
+    },
+
+    checkAnswer: function(userSelected){
+        if(this.currentQuestion.correctAnswer == userSelected){
+            console.log('Correct!');
+        }
+        else{
+            console.log('Incorrect!');
+        }
     }
 };
 
