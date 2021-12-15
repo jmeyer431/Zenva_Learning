6b483ef6c3d7d76cff913df114b34ec02220b9b3
Justin Meyer <justinmeyer_13@hotmail.com>
2021-12-15 11:11:46 -0600

    React Course-Lesson 4 'Hello World'

---
+++ React Course/react-hello/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function HelloWorld(){
+  return(
+    <div>Hello World!</div>
+  );
+}
+
+// function Hello(){
+//   return React.createElement(
+//     'div',
+//     {}
+//     'Hello World!'
+//   );
+// }
+
+ReactDOM.render(
+  <HelloWorld/>, document.querySelector('#root')
+);
